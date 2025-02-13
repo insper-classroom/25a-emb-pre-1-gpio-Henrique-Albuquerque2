74c89377bf71cf878440257c09ac24057a8c71a6
Henrique-Albuquerque2 <henriqueaa1@al.insper.edu.br>
2025-02-13 17:02:07 -0300

    exercicios 2,3,4

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,54 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_PIN_Vermelho = 28;
+const int BTN_PIN_Verde = 26;
+const int Led_vermelho = 4;
+const int Led_verde = 6;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(Led_vermelho);
+  gpio_set_dir(Led_vermelho, GPIO_OUT);
+
+  gpio_init(Led_verde);
+  gpio_set_dir(Led_verde, GPIO_OUT);
+
+  gpio_init(BTN_PIN_Vermelho);
+  gpio_set_dir(BTN_PIN_Vermelho, GPIO_IN);
+  gpio_pull_up(BTN_PIN_Vermelho);
+
+  gpio_init(BTN_PIN_Verde);
+  gpio_set_dir(BTN_PIN_Verde, GPIO_IN);
+  gpio_pull_up(BTN_PIN_Verde);
+
   while (true) {
+    if (!gpio_get(BTN_PIN_Vermelho)){
+      if (gpio_get(Led_vermelho) == 1){
+        gpio_put(Led_vermelho, 0);
+        printf("Botão vermelho pressionado\n");
+        while (!gpio_get(BTN_PIN_Vermelho)) {
+        };
+      } else {
+        gpio_put(Led_vermelho, 1);
+        printf("Botão vermelho pressionado\n");
+        while (!gpio_get(BTN_PIN_Vermelho)) {
+        }; 
+      }
+    }
+    if (!gpio_get(BTN_PIN_Verde)){
+      if (gpio_get(Led_verde) == 1){
+        gpio_put(Led_verde, 0);
+        printf("Botão verde pressionado\n");
+        while (!gpio_get(BTN_PIN_Verde)) {
+        };  
+      } else {
+        gpio_put(Led_verde, 1);
+        printf("Botão verde pressionado\n");
+        while (!gpio_get(BTN_PIN_Verde)) {
+        };
+      }
+    }
   }
 }
--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,49 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int red =5;
+const int roxo = 8;
+const int blue = 11;
+const int yellow = 15;
+
+const int botao = 28;
+
 int main() {
   stdio_init_all();
+  
+  gpio_init(red);
+  gpio_set_dir(red, GPIO_OUT);
 
+  gpio_init(roxo);
+  gpio_set_dir(roxo, GPIO_OUT);
+
+  gpio_init(blue);
+  gpio_set_dir(blue, GPIO_OUT);
+
+  gpio_init(yellow);
+  gpio_set_dir(yellow, GPIO_OUT);  
+  
+  gpio_init(botao);
+  gpio_set_dir(botao, GPIO_IN);
+  gpio_pull_up(botao);
+
+  
+  
   while (true) {
     // Use delay de 300 ms entre os estados!
+    if(!gpio_get(botao)){
+      gpio_put(red, 1);
+      sleep_ms(300);
+      gpio_put(red, 0);
+      gpio_put(roxo, 1);
+      sleep_ms(300);
+      gpio_put(roxo, 0);
+      gpio_put(blue, 1);
+      sleep_ms(300);
+      gpio_put(blue, 0);
+      gpio_put(yellow, 1);
+      sleep_ms(300);
+      gpio_put(yellow, 0);
+    }
   }
 }
